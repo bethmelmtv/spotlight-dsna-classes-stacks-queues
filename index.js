727c394c3ae939dbe03c09a3259c8041e01d8887
Beth <bethmelmtv@gmail.com>
2022-06-07 17:13:59 -0700

    worked on stack function and test

--- index.js
+++ index.js
@@ -1,5 +1,98 @@
-class Stack {}
+class Stack {
+  #list = [];
+
+  constructor(initialList) {
+    if (initialList) {
+      this.#list = initialList;
+    }
+  }
+  push(item) {
+    this.#list.push(item);
+  }
+
+  get readableList() {
+    return this.#list.toString();
+  }
+
+  pop(item) {
+    return this.#list.pop();
+  }
+
+  peek() {
+    return this.#list[this.#list.length - 1];
+  }
+}
 
 class Queue {}
 
 module.exports = { Stack, Queue };
+
+class MyDataStructure {
+  #list = [];
+
+  constructor(initialList) {
+    // invoked via: new MyDataStructure()
+    // not needed with private field initializers,
+    // unless you need to do other work
+    if (initialList) this.#list = initialList;
+  }
+
+  // methods
+
+  addBack(item) {
+    // access state, ie this.#list
+    this.#list.push(item);
+  }
+
+  addFront(item) {
+    // access state, ie this.#list
+    this.#list.unshift(item);
+  }
+
+  // property
+
+  get readableList() {
+    // access state, ie this.#list
+    return this.#list.toString();
+  }
+
+  get count() {
+    // access state, ie this.#list
+    return this.#list.length;
+  }
+
+  set count(count) {
+    // access state, ie this.#list
+    return (this.#list.length = count);
+  }
+}
+
+// used like:
+
+const myStructure = new MyDataStructure([4, 6, 8]);
+// use the defined methods and properties to
+// interact with this Abstract Data Structure:
+myStructure.addFront(3);
+myStructure.addFront(4);
+myStructure.addBack(7);
+myStructure.addBack(9);
+console.log(myStructure.count);
+// 7
+console.log(myStructure.readableList);
+// 4,3,4,6,8,7,9
+
+console.log(myStructure.Stack);
+
+const listOne = new MyDataStructure([2, 3, 4]);
+// use the defined methods and properties to
+// interact with this Abstract Data Structure:
+listOne.addFront(3);
+listOne.addFront(4);
+listOne.addBack(7);
+listOne.addBack(9);
+console.log(listOne.count);
+// 7
+console.log(listOne.readableList);
+// 4,3,4,6,8,7,9
+
+console.log(listOne.Stack);
--- index.test.js
+++ index.test.js
@@ -1,9 +1,28 @@
 const { Stack } = require("./index.js");
+
 describe("Stack Class", () => {
-  it("#push should add a new item to the stack ", () => {
-    const stack = new Stack();
-    stack.push(1);
-    expect(stack.peek()).toEqual(1);
+  it("#peek should return the top item without modifying ", () => {
+    const stack = new Stack([1]);
+    stack.push(2);
+    expect(stack.peek()).toEqual(2);
+    expect(stack.readableList).toEqual("1,2");
   });
-  // add more tests here...
+
+  it("#should initialize with  an existing list", () => {
+    const stack = new Stack([1, 2, 3]);
+    expect(stack.readableList).toEqual("1,2,3");
+  });
+
+  it("#push should add a new item to the stack", () => {
+    const stack = new Stack([1]);
+    stack.push(2);
+    expect(stack.readableList).toEqual("1,2");
+  });
+
+  it("#pop should add a new item to the stack", () => {
+    const stack = new Stack([1]);
+    stack.push(2);
+    expect(stack.pop()).toEqual(2);
+    expect(stack.readableList).toEqual("1");
+  });
 });
