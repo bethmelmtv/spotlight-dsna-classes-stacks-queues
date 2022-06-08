26034b5b5bf42244d14cdbfebb187dae6e11fbe4
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-07 17:59:37 -0700

    added queue and testing

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ class Stack {
     return this.#list.toString();
   }
 
-  pop(item) {
+  pop() {
     return this.#list.pop();
   }
 
@@ -23,76 +23,26 @@ class Stack {
   }
 }
 
-class Queue {}
-
-module.exports = { Stack, Queue };
-
-class MyDataStructure {
+class Queue {
   #list = [];
-
   constructor(initialList) {
-    // invoked via: new MyDataStructure()
-    // not needed with private field initializers,
-    // unless you need to do other work
-    if (initialList) this.#list = initialList;
+    if (initialList) {
+      this.#list = initialList;
+    }
   }
-
-  // methods
-
-  addBack(item) {
-    // access state, ie this.#list
-    this.#list.push(item);
-  }
-
-  addFront(item) {
-    // access state, ie this.#list
-    this.#list.unshift(item);
-  }
-
-  // property
-
   get readableList() {
-    // access state, ie this.#list
     return this.#list.toString();
   }
 
-  get count() {
-    // access state, ie this.#list
-    return this.#list.length;
+  enqueue(item) {
+    this.#list.push(item);
   }
-
-  set count(count) {
-    // access state, ie this.#list
-    return (this.#list.length = count);
+  dequeue() {
+    return this.#list.shift();
+  }
+  get hasNext() {
+    return this.#list.length > 0;
   }
 }
 
-// used like:
-
-const myStructure = new MyDataStructure([4, 6, 8]);
-// use the defined methods and properties to
-// interact with this Abstract Data Structure:
-myStructure.addFront(3);
-myStructure.addFront(4);
-myStructure.addBack(7);
-myStructure.addBack(9);
-console.log(myStructure.count);
-// 7
-console.log(myStructure.readableList);
-// 4,3,4,6,8,7,9
-
-console.log(myStructure.Stack);
-
-const listOne = new MyDataStructure([2, 3, 4]);
-// use the defined methods and properties to
-// interact with this Abstract Data Structure:
-listOne.addFront(3);
-listOne.addFront(4);
-listOne.addBack(7);
-listOne.addBack(9);
-console.log(listOne.count);
-// 7
-console.log(listOne.readableList);
-// 4,3,4,6,8,7,9
-
-console.log(listOne.Stack);
+module.exports = { Stack, Queue };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { Stack } = require("./index.js");
+const { Stack, Queue } = require("./index.js");
 
 describe("Stack Class", () => {
   it("#peek should return the top item without modifying ", () => {
@@ -24,5 +24,39 @@ describe("Stack Class", () => {
     stack.push(2);
     expect(stack.pop()).toEqual(2);
     expect(stack.readableList).toEqual("1");
+  });
+});
+
+describe("Queue class", () => {
+  it("should initialize with an empty list", () => {
+    const queue = new Queue();
+    expect(queue.readableList).toEqual("");
+  });
+
+  it("should initialize with an existing list", () => {
+    const queue = new Queue([1, 2, 3]);
+    expect(queue.readableList).toEqual("1,2,3");
+  });
+
+  it("#enqueue should add and item to the back of the queue", () => {
+    const queue = new Queue([1, 2, 3]);
+    queue.enqueue(4);
+    expect(queue.readableList).toEqual("1,2,3,4");
+  });
+
+  it("#dequeue should remove an item from thr from of the current list", () => {
+    const queue = new Queue([1, 2, 3]);
+    expect(queue.dequeue()).toEqual(1);
+    expect(queue.readableList).toEqual("2,3");
+  });
+
+  it("#hasNext should return true if there is something waiting", () => {
+    const queue = new Queue([1, 2, 3]);
+    expect(queue.hasNext).toEqual(true);
+  });
+
+  it("#hasNext should return false if there is nothing waiting", () => {
+    const queue = new Queue();
+    expect(queue.hasNext).toEqual(false);
   });
 });
